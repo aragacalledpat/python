6cc60371b4e8d21b1f9d51fcca7b18413f7b0ac5
Andrew <aragacalledpappas@gmail.com>
2015-05-04 22:57:52 -0400

    init the classes for graph exploration

---
+++ algorithmsReview/dfs.py
@@ -0,0 +1,11 @@
+from graph import DirectedGraph
+
+class DepthFirstSearch:
+    def __init__(self, graph, s):
+        print "init depth first search"
+        self.dfs(graph, s)
+
+    def dfs(self, graph, s):
+        print "lets do this"
+
+test = DepthFirstSearch(1,2)
