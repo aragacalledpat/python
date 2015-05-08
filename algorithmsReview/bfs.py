e08adc86bc33572d65e384d204bf0aef0b46ac56
Andrew <aragacalledpappas@gmail.com>
2015-05-07 22:15:46 -0400

    start on breadth first search

--- algorithmsReview/bfs.py
+++ algorithmsReview/bfs.py
@@ -1,1 +1,18 @@
+from graph import DirectedGraph
 
+class BreadthFirstSearch:
+    def __init__(self, graph, s):
+        print "init breadth first search"
+        self.bfs(graph, s)
+
+    def bfs(self, graph, s):
+        print "lets do this"
+
+testGrapth = DirectedGraph()
+testGrapth.addVertex("b",["a", "c"])
+testGrapth.addVertex("a",["c"])
+testGrapth.addVertex("c",["b"])
+
+
+
+test = BreadthFirstSearch(testGrapth,"a")
